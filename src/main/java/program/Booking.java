7540c5f73228f0103cce238123aa96880c19c9dd
MuhammadAfrizal <rizalpkl5758@gmail.com>
2026-07-09 16:02:19 +0700

    namabah model sama booking DAO

--- src/main/java/program/Booking.java
+++ src/main/java/program/Booking.java
@@ -1,19 +1,75 @@
 package program;
 
 public class Booking {
+
     private String idBooking;
-    private Peserta peserta;
-    private Jadwal jadwal;
+    private String idPeserta;
+    private String idJadwal;
+    private double harga;
     private String metodePembayaran;
     private String statusPembayaran;
 
+    public Booking(String idBooking,
+                   String idPeserta,
+                   String idJadwal,
+                   double harga,
+                   String metodePembayaran,
+                   String statusPembayaran) {
 
-
-    public Booking(String idBooking, Peserta peserta, Jadwal jadwal, String metodePembayaran, String statusPembayaran) {
         this.idBooking = idBooking;
-        this.peserta = peserta;
-        this.jadwal = jadwal;
+        this.idPeserta = idPeserta;
+        this.idJadwal = idJadwal;
+        this.harga = harga;
         this.metodePembayaran = metodePembayaran;
         this.statusPembayaran = statusPembayaran;
     }
+
+    public String getIdBooking() {
+        return idBooking;
+    }
+
+    public String getIdPeserta() {
+        return idPeserta;
+    }
+
+    public String getIdJadwal() {
+        return idJadwal;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public String getMetodePembayaran() {
+        return metodePembayaran;
+    }
+
+    public String getStatusPembayaran() {
+        return statusPembayaran;
+    }
+
+    public void setIdBooking(String idBooking) {
+        this.idBooking = idBooking;
+    }
+
+    public void setIdPeserta(String idPeserta) {
+        this.idPeserta = idPeserta;
+    }
+
+    public void setIdJadwal(String idJadwal) {
+        this.idJadwal = idJadwal;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    public void setMetodePembayaran(String metodePembayaran) {
+        this.metodePembayaran = metodePembayaran;
+    }
+
+    public void setStatusPembayaran(String statusPembayaran) {
+        this.statusPembayaran = statusPembayaran;
+    }
+
 }
--- src/main/java/program/BookingDAO.java
+++ src/main/java/program/BookingDAO.java
@@ -1,25 +1,156 @@
 package program;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class BookingDAO {
-    public boolean tambahBooking(Booking booking){
-        return false;
-    }
-
     public String tampilSemuaTransaksi(){
-        return null;
+        Connection con = Koneksi.getKoneksi();
+        StringBuilder hasil = new StringBuilder();
+        try {
+            String sql = """
+                SELECT
+                    b.id_booking,
+                    p.id_peserta,
+                    p.nama_lengkap,
+                    k.id_kelas,
+                    k.nama_kelas,
+                    j.id_jadwal,
+                    j.tanggal_mulai,
+                    b.harga,
+                    b.metode_pembayaran,
+                    b.status_pembayaran
+                FROM booking b
+                JOIN peserta p
+                    ON b.id_peserta = p.id_peserta
+                JOIN jadwal j
+                    ON b.id_jadwal = j.id_jadwal
+                JOIN kelas k
+                    ON j.id_kelas = k.id_kelas
+                ORDER BY b.id_booking DESC
+                """;
+
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            if (!rs.next()) {
+                return "";
+            }
+            String format = "%-4s %-8s %-20s %-8s %-20s %-10s %-15s %-12s %-12s %-10s\n";
+            hasil.append(String.format(format,
+                    "NO",
+                    "ID PST",
+                    "NAMA PESERTA",
+                    "ID KLS",
+                    "NAMA KELAS",
+                    "ID JWL",
+                    "TGL MULAI",
+                    "HARGA",
+                    "METODE",
+                    "STATUS"));
+
+            hasil.append("=========================================================================================================================================\n");
+            int no = 1;
+            do {
+                hasil.append(String.format(format,
+                        no++,
+                        rs.getString("id_peserta"),
+                        rs.getString("nama_lengkap"),
+                        rs.getString("id_kelas"),
+                        rs.getString("nama_kelas"),
+                        rs.getString("id_jadwal"),
+                        rs.getString("tanggal_mulai"),
+                        rs.getDouble("harga"),
+                        rs.getString("metode_pembayaran"),
+                        rs.getString("status_pembayaran")
+                ));
+            } while (rs.next());
+
+        } catch (Exception e) {
+            return "Error : " + e.getMessage();
+        }
+        return hasil.toString();
+
     }
 
-    public Booking cariById(String idBooking){
-        return null;
+    public boolean tambahBooking(Booking booking){
+        Connection con = Koneksi.getKoneksi();
+        try {
+
+            String sql = """
+                INSERT INTO booking
+                (id_booking,
+                id_peserta,
+                id_jadwal,
+                harga,
+                metode_pembayaran,
+                status_pembayaran)
+
+                VALUES (?,?,?,?,?,?)
+                """;
+
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            ps.setString(1, booking.getIdBooking());
+            ps.setString(2, booking.getIdPeserta());
+            ps.setString(3, booking.getIdJadwal());
+            ps.setDouble(4, booking.getHarga());
+            ps.setString(5, booking.getMetodePembayaran());
+            ps.setString(6, booking.getStatusPembayaran());
+
+            return ps.executeUpdate() > 0;
+
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+
+            return false;
+
+        }
     }
 
-    public boolean ubahStatusPembayaran(String idBooking,
-                                        String statusBaru){
-        return false;
+    // public Booking cariById(String idBooking){
+        
+    // }
+
+    public boolean ubahStatusPembayaran(String idBooking){
+        Connection con = Koneksi.getKoneksi();
+        try {
+            String sql = """
+                UPDATE booking
+                SET status_pembayaran='Lunas'
+                WHERE id_booking=?
+                """;
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, idBooking);
+            return ps.executeUpdate() > 0;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
     }
 
     public String generateIdBooking(){
-        return null;
+        Connection con = Koneksi.getKoneksi();
+        String id = "BK001";
+        try {
+            String sql = "SELECT MAX(id_booking) AS max_id FROM booking";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                String maxId = rs.getString("max_id");
+                if (maxId != null) {
+                    int angka = Integer.parseInt(maxId.substring(2));
+                    angka++;
+                    id = "BK" + String.format("%03d", angka);
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return id;
     }
 
 }
